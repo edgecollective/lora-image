cbc44a50e48e0187895fff6c56b7fddfdad24740
Don Blair <donblair@gmail.com>
2021-04-11 12:24:27 -0400

    update

--- ver-0.2/serial_receive.py
+++ ver-0.2/serial_receive.py
@@ -4,7 +4,7 @@ CHUNK_SIZE = 200
 #chunk_file=open('chunkfile_receive.png','wb+')
 
 chunk_file=open('image_in.bmp','wb+')
-with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
+with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
     ser.reset_input_buffer()
     while True:
         #print(ser.inWaiting())
