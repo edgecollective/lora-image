f06f651239e7addfb4bc0c948b41841cf11ce2d2
Don Blair <donblair@gmail.com>
2021-04-15 07:57:25 -0400

    copying into memory works!

---
+++ ver-0.8/serial_receive.py
@@ -0,0 +1,23 @@
+import serial
+import time
+CHUNK_SIZE = 200
+#chunk_file=open('chunkfile_receive.png','wb+')
+#serialPort = '/dev/ttyUSB0'
+serialPort = '/dev/ttyACM0'
+
+chunk_file=open('image_in.bmp','wb+')
+numBytesRead=0
+with serial.Serial(serialPort, 115200, timeout=1) as ser:
+    ser.reset_input_buffer()
+    while True:
+        #print(ser.inWaiting())
+        if(ser.inWaiting()>0):
+            #numBytesRead=numBytesRead+ser.inWaiting()
+            inBytes=ser.read(ser.inWaiting())
+            print(repr(inBytes))
+            #print(numBytesRead)
+            #print("\n")
+            chunk_file.write(inBytes)
+            chunk_file.flush()
+            #time.sleep(.1)
+            #chunk_file.close()
